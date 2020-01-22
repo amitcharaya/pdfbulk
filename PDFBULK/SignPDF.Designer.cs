2ed5c5e1bb7906d7642426c44e0e4db4f6ea5629
amitcharaya <charayaamit@gmail.com>
2020-01-22 20:03:32 +0530

    fixed

---
+++ PDFBULK/SignPDF.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace SIGNPDF
+{
+    partial class SignPDF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SignPDF));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputText = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.outputText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.heightTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.widthTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.yTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.xTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(26, 285);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sign";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(22, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Source";
+            // 
+            // inputText
+            // 
+            this.inputText.Location = new System.Drawing.Point(130, 26);
+            this.inputText.Name = "inputText";
+            this.inputText.Size = new System.Drawing.Size(320, 20);
+            this.inputText.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(476, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Select Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(476, 59);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Select Folder";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // outputText
+            // 
+            this.outputText.Location = new System.Drawing.Point(130, 56);
+            this.outputText.Name = "outputText";
+            this.outputText.Size = new System.Drawing.Size(320, 20);
+            this.outputText.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Destination";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.heightTextBox);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.widthTextBox);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.yTextBox);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.xTextBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(70, 100);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(380, 100);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Signature Box Size";
+            // 
+            // heightTextBox
+            // 
+            this.heightTextBox.Location = new System.Drawing.Point(274, 58);
+            this.heightTextBox.Name = "heightTextBox";
+            this.heightTextBox.Size = new System.Drawing.Size(62, 24);
+            this.heightTextBox.TabIndex = 22;
+            this.heightTextBox.Text = "20";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(208, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 18);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Up From";
+            // 
+            // widthTextBox
+            // 
+            this.widthTextBox.Location = new System.Drawing.Point(110, 55);
+            this.widthTextBox.Name = "widthTextBox";
+            this.widthTextBox.Size = new System.Drawing.Size(62, 24);
+            this.widthTextBox.TabIndex = 20;
+            this.widthTextBox.Text = "480";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(44, 58);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 18);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Start";
+            // 
+            // yTextBox
+            // 
+            this.yTextBox.Location = new System.Drawing.Point(274, 26);
+            this.yTextBox.Name = "yTextBox";
+            this.yTextBox.Size = new System.Drawing.Size(62, 24);
+            this.yTextBox.TabIndex = 18;
+            this.yTextBox.Text = "120";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(208, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 18);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "UP To";
+            // 
+            // xTextBox
+            // 
+            this.xTextBox.Location = new System.Drawing.Point(110, 23);
+            this.xTextBox.Name = "xTextBox";
+            this.xTextBox.Size = new System.Drawing.Size(62, 24);
+            this.xTextBox.TabIndex = 16;
+            this.xTextBox.Text = "560";
+            this.xTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.xTextBox_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 18);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "To";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.LightCyan;
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.progressBar1.Location = new System.Drawing.Point(26, 228);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(456, 23);
+            this.progressBar1.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(18, 318);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.Control;
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(386, 308);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(221, 23);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Developed by: Amit Kumar";
+            // 
+            // SignPDF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.BackgroundImage = global::SIGNPDF.Properties.Resources.image;
+            this.ClientSize = new System.Drawing.Size(619, 343);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.outputText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.inputText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SignPDF";
+            this.Text = "PDF BULK SIGNER";
+            this.Load += new System.EventHandler(this.SignPDF_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputText;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox outputText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox heightTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox widthTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox yTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox xTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ PDFBULK/SignPDF.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.security;
+
+
+
+using System.IO;
+
+using System.Security;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using iTextSharp.text;
+
+
+
+
+namespace SIGNPDF
+{
+    public partial class SignPDF : Form
+    {
+        double increment=0;
+        int progress;
+        public SignPDF()
+        {
+            InitializeComponent();
+            
+        }
+        public void sign(X509Certificate2 cert,String filename, String imageName, String output)
+        {
+            
+                Org.BouncyCastle.X509.X509CertificateParser cp = new Org.BouncyCastle.X509.X509CertificateParser();
+                Org.BouncyCastle.X509.X509Certificate[] chain = new Org.BouncyCastle.X509.X509Certificate[] {
+                cp.ReadCertificate(cert.RawData)};
+
+                IExternalSignature externalSignature = new X509Certificate2Signature(cert, "SHA-1");
+
+                PdfReader pdfReader = new PdfReader(filename);
+
+                FileStream signedPdf = new FileStream(output, FileMode.Create);
+
+                PdfStamper pdfStamper = PdfStamper.CreateSignature(pdfReader, signedPdf, '\0');
+                PdfSignatureAppearance signatureAppearance = pdfStamper.SignatureAppearance;
+                Image img=  Image.GetInstance(imageName);
+                img.Alignment = iTextSharp.text.Image.UNDERLYING;
+                signatureAppearance.SignatureGraphic = img;
+                
+                signatureAppearance.Acro6Layers = true;    
+                int x =Convert.ToInt32( xTextBox.Text);
+                int y = Convert.ToInt32(yTextBox.Text);
+                int w = Convert.ToInt32(widthTextBox.Text);
+                int h = Convert.ToInt32(heightTextBox.Text);
+                signatureAppearance.SetVisibleSignature(new iTextSharp.text.Rectangle(x,  y, w, h), pdfReader.NumberOfPages, "Signature");
+                signatureAppearance.SignatureRenderingMode = PdfSignatureAppearance.RenderingMode.GRAPHIC_AND_DESCRIPTION ;
+                    
+                MakeSignature.SignDetached(signatureAppearance, externalSignature, chain, null, null, null, 0, CryptoStandard.CMS);
+              
+            
+            }
+           
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                string inputPath = inputText.Text;
+                string path = AppDomain.CurrentDomain.BaseDirectory;
+                String image = path + "\\sign.jpg";
+                string output = outputText.Text;
+                string[] filePaths = Directory.GetFiles(inputPath, "*.pdf");
+                X509Store store = new X509Store(StoreLocation.CurrentUser);
+                store.Open(OpenFlags.ReadOnly);
+
+                X509Certificate2Collection sel = X509Certificate2UI.SelectFromCollection(store.Certificates, null, null, X509SelectionFlag.SingleSelection);
+
+                X509Certificate2 cert = sel[0];
+            
+                increment = filePaths.Length / 100.0;
+                if (increment<1)
+                {
+                    progress = (int)(100/(increment * 100));
+
+                }
+                else
+                {
+                    progress =(int) increment;
+                }
+
+
+
+              
+                for (int i = 0; i < filePaths.Length; i++)
+                {
+                    
+                    
+                    
+                    string fileName = Path.GetFileName(filePaths[i]);
+                    sign(cert, filePaths[i], image, output + "\\" + fileName);
+                    if (i % progress == 0)
+                    {
+                        
+                        progressBar1.Increment(1);
+                        
+
+                    }
+                    label7.Text = i+1 + " of " + filePaths.Length +" Signed" ;
+                    this.Refresh();
+                    
+                }
+                progressBar1.Value = 100;
+                
+                MessageBox.Show("All Files Signed Successfully");
+                progressBar1.Value = 0;
+              
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if(dialog.ShowDialog()==System.Windows.Forms.DialogResult.OK)
+            {
+                inputText.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                outputText.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void xTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar);
+        }
+
+        private void SignPDF_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        
+    }
+}
